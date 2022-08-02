ddeb08ce03fefbd64d2e8d9da20cceb0cebcfe71
Thinh The Nguyen <thinhtnguyen@kms-technology.com>
2022-08-02 20:57:09 +0700

    Remove SwiftJWT dependency

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ImagesListView",
-            dependencies: ["SwiftJWT"]),
+            dependencies: []),
         .testTarget(
             name: "ImagesListViewTests",
             dependencies: ["ImagesListView"]),
